daf9da881f25bd66867cce5c206c590138f1a70e
con-grieves <quickb3n@yahoo.co.uk>
2020-12-09 01:27:31 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from './logo-r.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="loginCard">
+        <div className="header">
+            <img className='Logo' src={logo} alt=""/>
+            <h3 className='exampleLoginText'>Example Login Screen</h3>
+            <p className='gettingStartedText'>Getting started with Green</p>         
+        </div>
+        <div className="email">
+            <p className='emailText'>Email Address</p>
+            <input className='emailInput' type="email"/> 
+        </div>    
+        <div className="checkBox">
+            <input className='tickBox' type="checkbox" name='saveDevice' defaultChecked/> 
+            <label className='saveDeviceText' htmlFor="saveDevice">Remember this Device</label> 
+        </div>
+        <button className='button' type='submit'>Sign In</button>
     </div>
   );
 }
