00addd29fa49c893c4ff6df3bb0d3ba426c7ef15
con-grieves <quickb3n@yahoo.co.uk>
2020-12-20 12:05:49 +0000

    Added validation - see old commit for original

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo-r.svg';
 import './App.scss';
 
+function submitClick() {
+  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.myForm.user_email.value)) {
+      alert("You're Signed In !");
+     } else {
+      alert("Invalid Email");
+     }  
+}
+
 function App() {
   return (
     <div className="loginCard">
@@ -9,17 +17,22 @@ function App() {
             <h3 className='exampleLoginText'>Example Login Screen</h3>
             <p className='gettingStartedText'>Getting started with Green</p>         
         </div>
-        <div className="email">
-            <p className='emailText'>Email Address</p>
-            <input className='emailInput' type="email"/> 
-        </div>    
-        <div className="checkBox">
-            <input className='tickBox' type="checkbox" name='saveDevice' defaultChecked/> 
-            <label className='saveDeviceText' htmlFor="saveDevice">Remember this Device</label> 
-        </div>
-        <button className='button' type='submit'>Sign In</button>
+        <form name="myForm" method="get">
+          <div className="email">
+              <p className='emailText'>Email Address</p>
+              <input className='emailInput' name="user_email" type="email"/> 
+          </div>    
+          <div className="checkBox">
+              <input className='tickBox' type="checkbox" name='saveDevice' defaultChecked/> 
+              <label className='saveDeviceText' htmlFor="saveDevice">Remember this Device</label> 
+         </div>
+          <button className='button' type='submit' onClick={submitClick}>Sign In</button>
+        </form>
     </div>
-  );
+   ) 
 }
 
+
+
 export default App;
+
